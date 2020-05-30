219cfeb1b843b8471bfa0ce0543bbd60891a273b
aumie <aum5376@gmail.com>
2020-05-30 14:11:51 +0700

    1

---
+++ hello/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <say-hello/hello.h>
+
+int main() {
+    std::cout << hello() << SAY_HELLO_VERSION << std::endl;
+    return 0;
+}
---
+++ say-hello/src/say-hello/hello.h
@@ -0,0 +1,7 @@
+#ifndef CPP_HELLO_H
+#define CPP_HELLO_H
+#include <string>
+
+std::string hello();
+
+#endif //CPP_HELLO_H
